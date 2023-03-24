07325b788602b249900ea9e8550cd9f396ddd2a0
Gautam Jain <astrogeek77@outlook.com>
2023-03-24 15:41:44 +0000

    Added dynamic data fetching and bootstrap support

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # import Flask class from flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from modules.jobs import JOBS
 
 app = Flask(__name__) # create Flask object
 # print(__name__) ->  # __main__
 
-# Response
-# res = ""
-
 # index route
 @app.route("/")
 def index():
-    return render_template("home.html");
+    return render_template("home.html", jobs=JOBS, company_name="Jester");
 
-
+# API for jobs
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True) # run flask app from within the main file
---
+++ modules/jobs.py
@@ -0,0 +1,26 @@
+JOBS = [
+  {
+    'id': 1,
+    'role': "IT Engineer",
+    'salary': "Rs. 12, 00, 000",
+    'location': 'Banglore, India',
+  },
+  {
+    'id': 2,
+    'role': "Frontend Engineer",
+    'salary': "Rs. 10, 00, 000",
+    'location': 'Remote',
+  },
+  {
+    'id': 3,
+    'role': "System Engineer",
+    'salary': "Rs. 8, 50, 000",
+    'location': 'Nodia, india',
+  },
+  {
+    'id': 4,
+    'role': "Backend Engineer",
+    'salary': "$. 150, 000",
+    'location': 'San Fancisco, USA',
+  },
+]
